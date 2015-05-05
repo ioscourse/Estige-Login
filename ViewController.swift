67084050c85ecac637fcec246a3dbf373956c988
Chuck Konkol <ckonkol@gmail.com>
2015-05-05 09:08:45 -0500

    Estige Login
    Estige Login

---
+++ ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Estige
+//
+//  Created by webstudent on 4/20/15.
+//  Copyright (c) 2015 Estige. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        let isLoggedIn:Int = prefs.integerForKey("ISLOGGEDIN") as Int
+        if (isLoggedIn != 1) {
+            self.performSegueWithIdentifier("goto_login", sender: self)
+        } else {
+            self.usernameLabel?.text = prefs.valueForKey("USERNAME") as? String
+        }
+    }
+
+    
+    
+    
+
+    @IBAction func logoutTapped(sender: UIButton) {
+        
+        let appDomain = NSBundle.mainBundle().bundleIdentifier
+        NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
+        
+        
+        self.performSegueWithIdentifier("goto_login", sender:self)
+        
+        
+        
+            
+        
+            
+        
+        }
+        
+        
+        
+    
+
+}
+
